b716f2c44ebd305d5d7ffcf2ea6582997219ca23
IGS <99769515+IonicGameStudios@users.noreply.github.com>
2022-03-08 10:55:25 -0500

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1315555.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 65825040, 65825041, 65825042, 65825043, 67450215 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/65825064/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/65825065/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/65825063/1/ammo.js', 'preload' : true},
+];
